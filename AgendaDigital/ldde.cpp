40826f16277c168518c94bc716135ed304622cb5
Alexandre Kenjy de Siqueira Kumagai <31069549+AlexandreKumagai@users.noreply.github.com>
2018-03-16 12:18:46 -0300

    implementação do inserir e imprimir(para testes)
    classe que usa o registro/no para criar uma lista com os conteudos do tipo T. Ela ainda não está completa, pois ao inserir o menor valor na lista, ele não está incluindo(problemas no if(!a)).

--- AgendaDigital/ldde.cpp
+++ AgendaDigital/ldde.cpp
@@ -1,6 +1,37 @@
 #include "ldde.h"
+#include "registro.cpp"
 
-LDDE::LDDE()
-{
+    Ldde(T valor){
+        primeiro->setConteudo(valor);
+    }
 
-}
+    bool inserir(T valor){
+        Registro<T> *a = new Registro<T>(NULL);//antes do primeiro
+        Registro<T> *b = primeiro;//primeiro
+        Registro<T> *novo = new Registro<T>(valor);
+            while(b && novo->getConteudo()> b->getConteudo()){
+                a=b;
+                b = b->getProximo();
+            }
+            if(!a){ // caso o valor seja o menor da fila
+                primeiro = novo;
+                primeiro->setProximo(b);
+                b->setAnterior(primeiro);
+                return true;
+            }
+            if(!b){//caso o valor a inserir seja o maior da fila
+                    a->setProximo(novo);
+                    novo->setAnterior(a);
+                    return true;
+                }
+    }
+    void imprimir(){
+        Registro<T> *b = primeiro;//primeiro
+        while(b){
+            cout<<"valor de b"<<b->getConteudo();
+            b = b->getProximo();
+        }
+
+    }
+};
+
