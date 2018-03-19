415746514225243eeef868bd8a6d7a0c06213887
Alexandre Kenjy de Siqueira Kumagai <31069549+AlexandreKumagai@users.noreply.github.com>
2018-03-19 11:52:21 -0300

    Correção dos métodos
    Métodos inserir, buscar e imprimir corrigidos, o método buscar, atualmente, só retorna o índice (inteiro) do conteúdo, e posteriormente, deverá retornar o conteúdo, a partir da data referenciada por parâmetro.

--- AgendaDigital/ldde.h
+++ AgendaDigital/ldde.h
@@ -10,53 +10,58 @@ class Ldde{
 private:
     Registro<T> *primeiro;
 public:
-    Ldde(){
-        primeiro = NULL;
-    }
-    Ldde(T valor){
-        primeiro = NULL;
-        primeiro->setConteudo(valor);
-    }
-
-    bool inserir(T valor){
-        Registro<T> *a = NULL;//antes do primeiro
-        Registro<T> *b = primeiro;//primeiro
-        Registro<T> *novo = new Registro<T>(valor);
-            while(b && novo->getConteudo()> b->getConteudo()){
-                a=b;
-                b = b->getProximo();
-            }
-            if(!a){ // caso o valor seja o menor da fila
-                primeiro = novo;
-                primeiro->setProximo(b);
-                b->setAnterior(primeiro);
-                return true;
-            }
-            if(!b){//caso o valor a inserir seja o maior da fila
+    	    Ldde(){
+	        primeiro = NULL;
+	        ultimo = NULL;
+	    }
+	    
+	    bool inserir(T valor){
+	        Registro<T> *a;//antes do primeiro
+	        a = NULL;
+	        Registro<T> *b = NULL;//primeiro
+	        b = primeiro;
+	        Registro<T> *novo = new Registro<T>(valor);
+	            while(b && novo->getConteudo()> b->getConteudo()){
+	                a=b;
+	                b = b->getProximo();
+	            }
+				if(a)
                     a->setProximo(novo);
-                    novo->setAnterior(a);
-                    return true;
-                }
-            return false;
-    }
-    void imprimir(){
-        Registro<T> *b = primeiro;//primeiro
-        while(b){
-            cout<<"valor de b"<<b->getConteudo();
-            b = b->getProximo();
-        }
+				else{ // caso o valor seja o menor da fila
+	                primeiro = novo;
+				}
+				
+	            if(b)//caso o valor a inserir seja o maior da fila
+	                b->setAnterior(novo);
+	           	else
+					ultimo = novo;
+					
+				novo->setProximo(b);
+				novo->setAnterior(a);
 
-    }
-    
-    Registro<T> *busca(T valor){
-         Registro<T> *b = primeiro;
-         while(b){
-             if(b->getConteudo()==valor)
-               return b;
-                                          
-          b=b->getProximo();
-         }                
-   }
+				return true;
+	    }
+	    void imprimir(){
+	        Registro<T> *b = primeiro;//primeiro
+			int i = 0;
+	        while(b){
+	            cout<<i<<" - valor e : "<<b->getConteudo()<<"\n";
+	            b = b->getProximo();
+	            i++;
+	        }
+	    }
+	    int buscar(T valor){
+	 		 Registro<T> *b = primeiro;
+	 		 int i = 0;
+	 		 while(b && b->getConteudo()){
+				if(b->getConteudo() == valor)
+				    return i;
+				b = b->getProximo();
+				i++;
+			}
+			return -1;
+		}
+
 };
 
 
