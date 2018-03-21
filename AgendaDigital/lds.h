beb93fe10be0408243873d899c65f8e9d2579b59
Igor_nasc <igor_flipe@hotmail.com>
2018-03-20 23:33:00 -0300

    Segunda versão da LDS
    Se possivel mais alguem teste para caso eu não enxergue algum erro.

--- AgendaDigital/lds.h
+++ AgendaDigital/lds.h
@@ -1,141 +1,114 @@
 #ifndef Lds_H
 #define Lds_H
-#include <iostream>
-#include <cstdlib>
-#include <stdexcept>
-#include <exception>
-#include <algorithm>
-
 template <class T>
-class Lds
-{private:
-    int n, alocacao;
-    T *vetor;
+class Lds{
+    private:
+        T* v;
+        int n;
+        int tamAloc;
 
-  public:
+    public:
+        Lds(){
+            n = 0;
+            tamAloc = 1;
+            v = new T[tamAloc];
+        }
+        bool inserir(T valor){
+            int i=0;
+            while(i<n && v[i]<valor)i++;
+            if(!empurra(i,valor))
+                return false;
+            n++;
+            return true;
+        }
+        int busca(T valor){
+            int ind = buscaRecursivaBinaria(0,n,valor);
+            if(ind != -1)
+                return ind;
+            cout<<"Não encontrado"<<endl;
+            return -1;
+        }
+        void imprimir(){
+            for(int i=0;i<n;i++)
+                cout<<i<<": "<<v[i]<<endl;
+        }
+        bool remover(T valor){
+            int i = busca(valor);
+            if(i < 0 || !puxa(i))
+                return false;
+            n--;
+            return true;
+        }
+        void removerInd(int i){
+            remover(v[i]);
+        }
+        T operator[](int i){
+            if(i>=n)
+                return v[0];
+           return v[i];
+        }
+        bool operator +(T valor){
+            return inserir(valor);
+        }
+        ~Lds(){
+            int i = 0;
+            while(i<n){
+                removerInd(i);
+                i++;
+            }
+        }
 
-      //construtor com uma assinatura
-      Lds(int aloc=100){
-          n=0;
-          this->alocacao = aloc;
-          this->vetor = new T[alocacao]; //alocando vetor do tipo T
-      }
-
-      /*
-      //construtor com duas assinaturas
-      Lds(T elemento, int quantidade){
-
-          n = 0;
-          this->alocacao = quantidade;
-          this->vetor = new T[alocacao]; //alocando vetor tipo T
-
-          for(int i =0; i<quantidade; i++){
-              Inserir(elemento);
-          }
-
-      }
-      */
-
-    bool Realocar(int nalc) { //fun��o que tem o papel de realocar
-      if (nalc <= n)
-        return false;
-
-      T *temp = new T[nalc];
-      if (temp == NULL){
-        return false;
-      }
-
-      copy(vetor, vetor + n, temp);
-      delete[] vetor;
-      vetor = temp;
-      return true;
-    }
-
-    //fun��o que insere dado ordenado na lista
-    bool InserirOrdenado(T novo) {
-
-      if (n >= alocacao) // verifica se � possivel inserir
-        if (!Realocar(2 * alocacao))
-          return false;
-
-      // buscando a posi��o para inserir conteudo
-      int i = 0;
-      for(i=0; i < n && vetor[i] < novo; i++);
-
-      for (int j = n; j > i; j--) {
-        vetor[j] = vetor[j - 1];
-      }
-
-      vetor[i] = novo;
-      n++;
-      return true;
-    }
-
-    int busca(T elemento) { //busca linear
-      for (int i = 0; i < n; i++) {
-        if (vetor[i] == elemento)
-          return i;
-      }
-      return -1;
-    }
-
-    int buscaBinaria(T elemento) { //busca binaria
-      int i = 0;
-      int f = n - 1;
-      while (i <= f) {
-        int p = (i + f) / 2;
-        if (elemento < vetor[p])
-          f = p - 1;
-        else if (elemento > vetor[p])
-          i = p + 1;
-        else
-          return p;
-      }
-
-      return -1;
-    }
-
-    bool remove(T elemento) { // fun��o que remove elemento da lista
-      int pos = busca(elemento);
-      if (pos == -1)
-        return false;
-
-      for (int i = pos; i < n - 1; i++)
-        vetor[i] = vetor[i + 1];
-
-      n--;
-
-      if (n <= alocacao / 2 && n + 1 >= 10)
-        Realocar(n + 1);
-
-      return true;
-    }
-
-      void Imprimir(){ // imprime elementos da lista
-
-      for(int i=0; i<n; i++)
-          cout << vetor[i] <<endl;
-    }
-
-    void operator=(const Lds<T> &igl) {
-      n = 0;
-      for (int i = 0; i < igl.n; i++) {
-        Inserir(igl[i]);
-      }
-    }
-
-    const T &operator[](int a) const {
-      if (a >= n) {
-        throw std::runtime_error("Deu ruim!!!!!Estourou");
-        return vetor[0];
-      }
-      return vetor[a];
-    }
-
-    int size() const {
-      return n;
-    }
-
+    private:
+        void copiar(T* v, T*v2){
+            for(int i=0;i<n;i++)
+                v2[i] = v[i];
+        }
+        bool realoca(){
+            T* v2 = new T[tamAloc*2];
+            if(!v2)
+                return false;
+            tamAloc = tamAloc*2;
+            copiar(v,v2);
+            return true;
+        }
+        bool desaloca(){
+            int tamAloc2 = tamAloc/2;
+            T* v2 = new T[tamAloc2];
+            if(!v2)
+                return false;
+            tamAloc = tamAloc2;
+            copiar(v,v2);
+            return true;
+        }
+        bool empurra(int i,T valor){
+            if(n >= tamAloc)
+                if(!realoca())
+                    return false;
+            int j=0;
+            for(j=n ; j>i; j=j-1)
+                v[j] = v[j-1];
+            v[i] = valor;
+            return true;
+        }
+        bool puxa(int i){
+            if(n < tamAloc/2)
+                if(!desaloca())
+                    return false;
+            int j=0;
+            for(j=i ; j<n;j++)
+                v[j] = v[j+1];
+            return true;
+        }
+        int buscaRecursivaBinaria(int i, int f, T valor){
+            if(i>f)
+                return -1;
+            int p = (i+f)/2;
+            if(v[p] == valor)
+                return p;
+            if(v[p] < valor)
+                return buscaRecursivaBinaria(p+1,f,valor);
+            else
+                return buscaRecursivaBinaria(i,p-1,valor);
+        }
 };
-
 #endif // Lds_H
--- AgendaDigital/main.cpp
+++ AgendaDigital/main.cpp
@@ -7,24 +7,44 @@
 
 int main(int argc, char *argv[])
 {
+    Lds<int> teste;
+
+    teste.inserir(10);
+    teste.inserir(1);
+    teste.remover(5);
+    teste.remover(1);
+    teste.remover(10);
+    teste.imprimir();
+    cout<<endl;
+
+    for(int i=10;i>=0;i--)
+        teste.inserir(i);
+
+    cout<<teste.remover(10)<<endl;
+    cout<<teste.remover(101)<<endl;
+    //cout<<teste.busca(100)<<endl;
+    //cout<<teste.busca(101)<<endl;
+    teste.imprimir();
+
+//   MainWindow w;
+//   w.show();
+//   Ldde<int> teste;
+//   teste.inserir(10);
+//   teste.inserir(50);
+//   teste.inserir(1);
+
+//   teste.busca(1);
+
+//   teste.Remove(2);
+//   teste.Remove(10);
+//   teste.Remove(50);
+//   teste.Remove(1);
+
+//   teste.inserir(5);
+
+//   teste.imprimir();
+
+    return 0;
     //QApplication aa(argc, argv);
-    //MainWindow w;
-    //w.show();
-   Ldde<int> teste;
-   teste.inserir(10);
-   teste.inserir(50);
-   teste.inserir(1);
-
-   //teste.busca(1);
-
-   teste.Remove(2);
-   teste.Remove(10);
-   teste.Remove(50);
-   //teste.Remove(1);
-
-   teste.inserir(5);
-
-   teste.imprimir();
-   //return aa.exec();
-   return 0;
+    //return aa.exec();
 }
