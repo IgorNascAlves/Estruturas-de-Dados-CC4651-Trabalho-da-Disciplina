4f49899702335460c266d20534c4ad93bfa651b1
Augusto Turolla <gutoturolla@hotmail.com>
2018-03-31 22:17:41 -0300

    Update compromisso.h

--- AgendaDigital/compromisso.h
+++ AgendaDigital/compromisso.h
@@ -1,4 +1,75 @@
-#ifndef DIA_H
-#define DIA_H
+#ifndef compromisso_H
+#define compromisso_H
+#include "ldde.h"
 
-#endif // DIA_H
+
+class Compromisso
+{
+
+private:
+
+    string Titulo;
+    string Descricao;
+    int Dia;
+    int Mes;
+    int Ano;
+
+public:
+    Compromisso() {
+        Titulo = 0;
+        Descricao = 0;
+        Dia = 0;
+        Mes = 0;
+        Ano = 0;
+    }
+
+    bool setMes(int i){
+        if(i<1 && i>12)
+            return false;
+        Mes = i;
+        return true;
+
+    }
+
+    int getMes(){
+        return Mes;
+    }
+
+
+    void setDia(int i){
+        Dia = i;
+
+    }
+    int getDia(){
+        return Dia;
+    }
+    void setAno(int i){
+        Ano = i;
+
+    }
+    int getAno(){
+        return Ano;
+    }
+
+    void setTitulo(string a){
+        Titulo = a;
+    }
+    string getTitulo(){
+        return Titulo;
+    }
+    void setDescricao(string a){
+        Descricao = a;
+    }
+    string getDescricao(){
+        return Descricao;
+    }
+
+
+
+
+
+
+};
+
+
+#endif // compromisso_H
