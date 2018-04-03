0f9957bc920f94f96e06fca219da8091b07ae6b6
Alexandre Kenjy de Siqueira Kumagai <alexandrekenjy@gmail.com>
2018-04-03 14:11:00 -0300

    Organização do arq .cpp para .h
    Os dados da função pessoa.cpp era para estar em pessoa.h, arrumado também as funções gets que estavam tipo void

--- AgendaDigital/pessoa.h
+++ AgendaDigital/pessoa.h
@@ -1,12 +1,54 @@
 #ifndef PESSOA_H
 #define PESSOA_H
+#include "ldde.h"
 
+using namespace std;
 
-class Pessoa
-{
-public:
+class Pessoa{
+
+private:
     string nome;
-    Pessoa();
-};
+    string login;
+    string senha;
+    Ldde <Compromisso> tarefas;
+public:
+    Pessoa(): nome(""), login(""), senha(""){
+    }
+    Pessoa(string n, string l, string s){
+        nome = n;
+        login= l;
+        senha = s;
+    }
 
+    Ldde<Compromisso> getTarefas(){
+        return tarefas;
+    }
+
+
+    string getNome(){
+        return nome;
+    }
+
+    string setNome(string nome){
+        this->nome = nome;
+    }
+
+    string getLogin(){
+        return login;
+    }
+
+    string setLogin(string login){
+        this->login = login;
+    }
+
+    string getSenha(){
+        return senha;
+    }
+
+    string setSenha(string senha){
+        this->senha = senha;
+    }
+
+
+};
 #endif // PESSOA_H
