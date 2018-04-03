c9b229ef5bfbc5cdea429bcb15db331c8cd55fb9
Alexandre Kenjy de Siqueira Kumagai <alexandrekenjy@gmail.com>
2018-04-03 14:16:56 -0300

    testes no main
    Apenas testes para o main, um teste apenas para ldde e compromisso, o outro para lddecompromisso e pessoa

--- AgendaDigital/main.cpp
+++ AgendaDigital/main.cpp
@@ -5,20 +5,26 @@
 #include "lds.h"
 #include "pessoa.h"
 
-
 int main(int argc, char *argv[])
 {
-    Ldde<int> teste;
-    for(int i=10;i>=0;i--)
-        teste.inserir(i);
+    /*
+    Ldde<Compromisso> teste;
+    teste.inserir(Compromisso("dormir", "Soocorro preciso dormir", 20, 4, 2018));
+    teste.inserir(Compromisso("ir a FEI", "Fazer o q ne?", 12, 4, 2018));
+    teste.inserir(Compromisso("Ir a feira", "Vender pastel de flango", 23, 4, 2018));
+    teste.inserir(Compromisso("Casa das prima", "Ninguem e de ferro", 7, 5, 2018));
+    teste.inserir(Compromisso("Ir a fei", "aula do tio patinhas", 23, 3, 2018));
+    teste.inserir(Compromisso("Cinema", "guerra civil", 7, 2, 2018));
     teste.imprimir();
-    for(int i=0;i<10;i++)
-        teste.Remove(i);
+    */
     //teste.Remove(10);
     //puts("");
     //teste.imprimir();
     //teste.inserir(5);
     //teste.imprimir();
+    Pessoa x;
+    x.getTarefas().inserir(Compromisso("Cinema", "guerra civil",10, 10, 2030));
+    x.getTarefas().imprimir();
 
     //return 0;
 
