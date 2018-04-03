2410e9ce84d1182cafcb0323a006b4124fe5cb5a
Alexandre Kenjy de Siqueira Kumagai <alexandrekenjy@gmail.com>
2018-04-03 20:12:31 -0300

    alteração main
    alteração no main o x.getTarefas agora usa -> para acessar as funções

--- AgendaDigital/main.cpp
+++ AgendaDigital/main.cpp
@@ -4,32 +4,36 @@
 #include "ldde.h"
 #include "lds.h"
 #include "pessoa.h"
-
+#include <iostream>
+#include <cstdlib>
 int main(int argc, char *argv[])
 {
-    /*
+
     Ldde<Compromisso> teste;
     teste.inserir(Compromisso("dormir", "Soocorro preciso dormir", 20, 4, 2018));
     teste.inserir(Compromisso("ir a FEI", "Fazer o q ne?", 12, 4, 2018));
-    teste.inserir(Compromisso("Ir a feira", "Vender pastel de flango", 23, 4, 2018));
-    teste.inserir(Compromisso("Casa das prima", "Ninguem e de ferro", 7, 5, 2018));
+    teste.inserir(Compromisso("Ir a feira", "Vender pastel", 23, 4, 2018));
+    teste.inserir(Compromisso("Dormir mais", "Ninguem e de ferro", 7, 5, 2018));
     teste.inserir(Compromisso("Ir a fei", "aula do tio patinhas", 23, 3, 2018));
     teste.inserir(Compromisso("Cinema", "guerra civil", 7, 2, 2018));
     teste.imprimir();
-    */
-    //teste.Remove(10);
-    //puts("");
-    //teste.imprimir();
+    cout<<"-------------------------------------------------\n";
+    teste.remove("Ir a feira");
+
+    teste.imprimir();
     //teste.inserir(5);
     //teste.imprimir();
+    /*
     Pessoa x;
-    x.getTarefas().inserir(Compromisso("Cinema", "guerra civil",10, 10, 2030));
-    x.getTarefas().imprimir();
+    x.getTarefas()->inserir(Compromisso("Cinema", "guerra civil",10, 10, 2030));
+    x.getTarefas()->imprimir();
+    */
+    return 0;
 
-    //return 0;
-
+    /*
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
+    */
 }
